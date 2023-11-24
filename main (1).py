13cba27cd3b587b6a50731fc1c2ffdac1c246e0c
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 00:07:17 +0600

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+import random
+import choice
+name = input("What is your name?: ")
+print(f"Hello {name}! I am Stella.")
+time.sleep(0)
+print("I  am an Ai. bot and I can predict your future.")
+b = input("Ask me a question: ")
+time.sleep(0)
