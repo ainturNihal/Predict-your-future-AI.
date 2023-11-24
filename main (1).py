68d0703037931e12628354b6415eeb6bf8dc73a6
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 00:15:04 +0600

    Update main (1).py
    Some works are still processing

--- main (1).py
+++ main (1).py
@@ -1,3 +1,4 @@
+##Still under construction
 import random
 import choice
 name = input("What is your name?: ")
@@ -6,3 +7,15 @@ time.sleep(0)
 print("I  am an Ai. bot and I can predict your future.")
 b = input("Ask me a question: ")
 time.sleep(0)
+v = 10%,30%,40%
+a = 60%,80$,90%,99%
+first_half = random.choice(v)
+second_half = random.choice(a)
+time.sleep(0)
+print("Wait...")
+time.sleep(0)
+print(f"{first_half} done")
+time.sleep(0)
+print("Seeing all possibilities...")
+print(f"{seconf_half} done")
+time.sleep("Checking all the possible out comes!")
